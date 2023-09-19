4c0b41fde4ad4308795994880e05c5c43aec853a
OGOZ111 <djsmallboy@gmail.com>
2023-09-19 13:15:46 +0300

    added margins for scroll

--- CSS class/main.js
+++ CSS class/main.js
@@ -1,6 +1,8 @@
 const backButton = document.querySelector('#backToTop');
 const mobButton = document.querySelector('.mobile')
 const nav = document.querySelector('nav ul')
+const menuItems = document.querySelectorAll('nav ul li a')
+const header = document.querySelector('header')
 
 window.onscroll = function() {scrollFunction()};
 
@@ -11,6 +13,11 @@ function scrollFunction() {
     } else {
       backButton.style.display = "none";
     }
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        header.classList.add('bg');
+      } else {
+        header.classList.remove('bg');
+      }
   }
 
 const getToTop = () => {
@@ -19,6 +26,10 @@ const getToTop = () => {
 }
 
 const mobMenu = () => {
+    for (const item of menuItems) {
+        item.addEventListener('click', mobMenu)
+    }
+
     if (nav.classList.contains('responsive')){
         nav.classList.remove('responsive')
     } else {nav.classList.add('responsive')}
