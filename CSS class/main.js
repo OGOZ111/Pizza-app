e87a70c9a27aea3b73dbced6e7f8e86046965eea
OGOZ111 <djsmallboy@gmail.com>
2023-09-19 12:59:48 +0300

    added burger menu

--- CSS class/main.js
+++ CSS class/main.js
@@ -1,8 +1,29 @@
 const backButton = document.querySelector('#backToTop');
+const mobButton = document.querySelector('.mobile')
+const nav = document.querySelector('nav ul')
+
+window.onscroll = function() {scrollFunction()};
+
+// w3 schools, 2 different code for different browser engines. 
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      backButton.style.display = "block";
+    } else {
+      backButton.style.display = "none";
+    }
+  }
 
 const getToTop = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
+const mobMenu = () => {
+    if (nav.classList.contains('responsive')){
+        nav.classList.remove('responsive')
+    } else {nav.classList.add('responsive')}
+
+}
+
+mobButton.addEventListener('click', mobMenu)
 backButton.addEventListener('click', getToTop)
